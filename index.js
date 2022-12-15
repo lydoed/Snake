cea586827f093883a0563f0fe0601681b35d99f8
lydoed <97830280+lydoed@users.noreply.github.com>
2022-12-15 04:34:48 +0200

    Snake

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const __dirname = path.resolve()
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.urlencoded({extended : false}))
